587ed262eafbdd00632b00b64f59c558c695ec0f
NikhilVermaDataScience <165783644+NikhilVermaDataScience@users.noreply.github.com>
2024-05-03 14:01:56 +0530

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,22 @@
+"""
+Comments = Comments in Python are the lines in the code that are ignored by the interpreter
+during the execution of the program
+Types of Comments in Python
+1.Single line Comments
+2.Multiline Comments
+"""
+# 1.Single line Comments
+print("hello")
+# print("hello 2"
+# 2.Multiline Comments
+# First type = use '#' in every line like
+#hi
+#Yo
+# Second type = use ''' like
+'''
+Hi.
+'''
+# thread type = use """ like
+"""
+Hi.
+"""
